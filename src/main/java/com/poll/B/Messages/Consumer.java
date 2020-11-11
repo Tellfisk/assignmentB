5fd4d5bc0b88db51c29901711fac578956fbd4e1
Jonas Haneborg <jon.haneborg@gmail.com>
2020-11-11 14:43:59 +0100

    Producer and Consumer init.

--- src/main/java/com/poll/B/Messages/Consumer.java
+++ src/main/java/com/poll/B/Messages/Consumer.java
@@ -1,4 +1,21 @@
 package com.poll.B.Messages;
 
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
 public class Consumer {
+    public static void main(String [] args) throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+
+        try (Connection connection = factory.newConnection()) {
+            Channel channel = connection.createChannel();
+            channel.queDeclare("queue-name", false, false, false, null);
+
+            String message = "message";
+
+            channel.basicPublish("", "queue-name", false, null, message.getBytes());
+            System.out.println("Message sent");
+        }
+    }
+
 }
